97bfba443f1f4d3b16a5d2bb6d981284ec055784
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2022-05-11 15:19:56 +0200

    Added comments

--- src/main/java/se/rmsit/TicTacToe/exceptions/InvalidCoordinatesException.java
+++ src/main/java/se/rmsit/TicTacToe/exceptions/InvalidCoordinatesException.java
@@ -1,4 +1,5 @@
 package se.rmsit.TicTacToe.exceptions;
 
 // Exception för ogiltiga koordinater, det vill säga om koordinaterna ligger utanför spelplanen.
+// Använder RuntimeException eftersom felet inte ska kunna ske om korrekt data skickas.
 public class InvalidCoordinatesException extends RuntimeException {}
--- src/main/java/se/rmsit/TicTacToe/exceptions/InvalidPlayerTypeException.java
+++ src/main/java/se/rmsit/TicTacToe/exceptions/InvalidPlayerTypeException.java
@@ -1,4 +1,5 @@
 package se.rmsit.TicTacToe.exceptions;
 
 // Anger felaktig spelartyp vid drag (endast x och o är giltiga)
+// Använder RuntimeException eftersom felet inte ska kunna ske om korrekt data skickas.
 public class InvalidPlayerTypeException extends RuntimeException {}
